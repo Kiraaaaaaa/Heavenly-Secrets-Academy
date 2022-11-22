58f674f0edfcb7dafa110d22397dbaf6f38f2894
虎哥 <huyi0612@163.com>
2022-11-22 16:53:35 +0800

    Jwt工具注释

--- tj-auth/tj-auth-service/src/main/java/com/tianji/auth/service/impl/AccountServiceImpl.java
+++ tj-auth/tj-auth-service/src/main/java/com/tianji/auth/service/impl/AccountServiceImpl.java
@@ -2,15 +2,12 @@ package com.tianji.auth.service.impl;
 
 import com.tianji.api.client.user.UserClient;
 import com.tianji.api.dto.user.LoginFormDTO;
-import com.tianji.auth.common.constants.JwtConstants;
 import com.tianji.auth.service.IAccountService;
 import com.tianji.auth.service.ILoginRecordService;
 import com.tianji.auth.util.JwtTool;
 import com.tianji.common.domain.dto.LoginUserDTO;
 import com.tianji.common.exceptions.BadRequestException;
-import com.tianji.common.utils.BooleanUtils;
 import com.tianji.common.utils.UserContext;
-import com.tianji.common.utils.WebUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -48,7 +45,7 @@ public class AccountServiceImpl implements IAccountService{
         // 2.1.设置记住我标记
         detail.setRememberMe(loginDTO.getRememberMe());
         // 2.2.生成token
-        String token = generateTokens(detail);
+        String token = jwtTool.createToken(detail);
 
         // 3.计入登录信息表
         loginRecordService.loginSuccess(loginDTO.getCellPhone(), detail.getUserId());
@@ -60,24 +57,5 @@ public class AccountServiceImpl implements IAccountService{
     public void logout() {
         // 删除jti
         jwtTool.cleanJtiCache();
-    }
-
-    private String generateTokens(LoginUserDTO userDTO) {
-        // 1.生成access-token
-        String accessToken = jwtTool.createToken(userDTO);
-        // 2.生成refresh-token
-        String refreshToken = jwtTool.createRefreshToken(userDTO);
-        // 3.写入cookie
-        // 3.1.如果是记住我，则cookie有效期7天，否则 -1，当前会话有效
-        int maxAge = BooleanUtils.isTrue(userDTO.getRememberMe()) ?
-                (int) JwtConstants.JWT_REMEMBER_ME_TTL.toSeconds() : -1;
-        // 3.2.生成cookie, 用户登录和管理员登录的刷新token区分一下
-        WebUtils.cookieBuilder()
-                .name(JwtConstants.REFRESH_HEADER)
-                .value(refreshToken)
-                .maxAge(maxAge)
-                .httpOnly(true)
-                .build();
-        return accessToken;
     }
 }
